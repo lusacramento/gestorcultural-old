2d128ea5748f1a7c76c0db89f86bae1431a39c87
Lu Sacramento <lucianossacramento@yahoo.com.br>
2023-01-25 00:08:47 -0300

    init springframework

---
+++ backend/src/main/java/br/com/gerenciadordeprojetosculturais/gerenciadordeprojetosculturais/GerenciadorDeProjetosCulturaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadordeprojetosculturais.gerenciadordeprojetosculturais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeProjetosCulturaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeProjetosCulturaisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gerenciadordeprojetosculturais/gerenciadordeprojetosculturais/GerenciadorDeProjetosCulturaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadordeprojetosculturais.gerenciadordeprojetosculturais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeProjetosCulturaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
