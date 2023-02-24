8ce0043f377951c1f25befb0eb1f9add45dc5803
Lu Sacramento <lucianossacramento@yahoo.com.br>
2023-02-24 20:21:09 -0300

    front custom index page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,20 @@
 <template>
 	<NuxtLayout>
-		<h1>Index</h1>
+		<main class="container align-items-center d-flex">
+			<h1>Index</h1>
+			<div
+				class="d-flex row justify-content-center text-center align-items-center"
+			>
+				<div class="col-12"></div>
+			</div>
+		</main>
 	</NuxtLayout>
 </template>
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+	main {
+		height: 80vh;
+	}
+</style>
