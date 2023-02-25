1f95a3d0e2290bafdf8cb3121dd3e96f38da24a7
Lu Sacramento <lucianossacramento@yahoo.com.br>
2023-02-25 11:09:44 -0300

    front implements register user

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,20 +1,16 @@
 <template>
-	<NuxtLayout>
-		<main class="container align-items-center d-flex">
-			<h1>Index</h1>
-			<div
-				class="d-flex row justify-content-center text-center align-items-center"
-			>
-				<div class="col-12"></div>
-			</div>
-		</main>
-	</NuxtLayout>
+  <NuxtLayout>
+    <main class="container align-items-center d-flex">
+      <h1>Index</h1>
+      <div class="d-flex row justify-content-center text-center align-items-center">
+        <div class="col-12"></div>
+      </div>
+    </main>
+  </NuxtLayout>
 </template>
 
-<script setup></script>
-
 <style scoped>
-	main {
-		height: 80vh;
-	}
+main {
+  height: 80vh;
+}
 </style>
